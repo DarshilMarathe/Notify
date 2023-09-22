b2a680f48312a9ad877d16575072db267b16efdf
Darshil Marathe <99969644+DarshilMarathe@users.noreply.github.com>
2023-09-22 13:44:55 +0530

    Removed useContext learning

--- notify/src/context/notes/NoteState.js
+++ notify/src/context/notes/NoteState.js
@@ -3,26 +3,12 @@ import noteContext from "./noteContext";
 
 const NoteState = (props) =>{
 
-    const s1 = {
-        "name":"Darshil",
-        "class" : "11"
-    }
 
-    const [state, setstate] = useState(s1);
-
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name":"Larry",
-                "class":"0"
-            })
-        }, 1000);
-    }
 
 
     return(
         // <noteContext.Provider value ={{state : state,update : update}}>   --same 
-        <noteContext.Provider value ={{state,update}}> 
+        <noteContext.Provider value ={{}}> 
             {props.children}
         </noteContext.Provider>
     )
